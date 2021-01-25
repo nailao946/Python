fd5c0acfef9fc697ac13fd9bd25f191bc5be1cc2
nailao946 <1499964238@qq.com>
2021-01-25 17:04:09 +0800

    python学习

---
+++ sort的使用.py
@@ -0,0 +1,14 @@
+student = [{'name': 'xindao', 'age': 17},
+           {'name': 'hutao', 'age': 16},
+           {'name': 'ziyou', 'age': 18},
+           {'name': 'xiandao', 'age': 30},
+           ]
+
+
+def x(sort):
+    return sort['age']
+
+
+student.sort(key=x)
+# 如果给定了键函数，则对每个列表项应用一次，并根据它们的函数值升序或降序排序。
+print(student)
