2098f69d254fc7c60b592a94b96fcfaaf575520b
nailao946 <1499964238@qq.com>
2021-01-25 17:15:33 +0800

    python学习

--- sort的使用.py
+++ sort的使用.py
@@ -10,5 +10,21 @@ def x(sort):
 
 
 student.sort(key=x)
+
 # 如果给定了键函数，则对每个列表项应用一次，并根据它们的函数值升序或降序排序。
 print(student)
+#
+#
+#
+# calc = [{'num': 3},
+#         {'num': 2},
+#         {'num': 4},
+#         {'num': 5}
+#         ]
+#
+#
+# def adition(x):
+#     print(1 + x['num'])
+#
+#
+# calc.sort(key=adition)
